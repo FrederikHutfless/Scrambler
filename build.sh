74cb4988f78cdff7ee62d37d2ca4fbf41aa48e52
CMOS <system@speed-cmos.com>
2021-05-24 21:22:09 +0200

    Restructure Folders

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 echo "Building scrambler."
-rm -f scrambler.js
-rm -f scrambler.min.js
-cat aa_start.js changeNotation.js minx.js cuboid.js other.js changeImageColors.js addColor.js pyr_rep.js nnn_representation.js scramble_special_222.js c123.js drawWCA.js random_move_nnn.js zz_end.js scramble_wca.js scramble_subset.js > scrambler.js
+rm -f built/scrambler.js
+rm -f built/scrambler.min.js
+cat aa_start.js changeNotation.js minx.js cuboid.js other.js changeImageColors.js addColor.js representations/pyr_rep.js representations/nnn_representation.js scramble_special_222.js representations/c123.js drawWCA.js random_move_nnn.js zz_end.js libs/scramble_wca.js libs/scramble_subset.js > built/scrambler.js
 echo "Minifying"
-npx esbuild scrambler.js --target=es6 --outfile=scrambler.min.js --minify
+npx esbuild built/scrambler.js --target=es6 --outfile=built/scrambler.min.js --minify
 echo "Done."
-stat --printf="Unminified %s\n" scrambler.js
-stat --printf="Minified   %s\n" scrambler.min.js
+stat --printf="Unminified %s bytes\n" built/scrambler.js
+stat --printf="Minified   %s bytes\n" built/scrambler.min.js
